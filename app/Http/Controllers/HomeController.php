5bd5598c5e86dd8003d0b2316308396756893ef5
FelixMerchan <56200776+FelixMerchan@users.noreply.github.com>
2019-10-12 11:33:47 -0500

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,12 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    /*public function index()
     {
         return view('home');
+    }
+    */
+    public function administracion(){
+        return view('administracion.index');
     }
 }
