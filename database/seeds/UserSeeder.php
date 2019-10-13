df053eaf7ab252c6a5bd9084ae5b1ea8ad0344c0
FelixMerchan <56200776+FelixMerchan@users.noreply.github.com>
2019-10-13 09:47:24 -0500

    datos por defectos

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+
+            'id'=>'1',
+            'name'=>'Super Administrador',
+            'estado'=> 1,
+            'email'=> 'elvis_merchan@hotmail.com',
+            'password'=> bcrypt('26e04f92'),
+            'email_verified_at'=>"2019-04-01 12:31:15",
+
+        ]);
+    }
+}
